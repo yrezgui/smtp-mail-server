1e6c70da35f6b814e13317e5c4a9359487679c7f
Yacine Rezgui <rezgui.y@gmail.com>
2015-09-09 16:01:49 +0100

    Listen to the port 3333 for the web server

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 require('babel/register');
 
 var app = require('./src');
+
+var server = app.listen(3333, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
