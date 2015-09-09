91df5af723b1271345db6dbcc70762295e0df81c
Yacine Rezgui <rezgui.y@gmail.com>
2015-09-09 16:01:13 +0100

    Add cache and web server to display emails

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import mailin from 'mailin';
+import express from 'express';
+import LRU from 'lru-cache';
+
+let app   = express();
+let cache = LRU({
+  max: 50,
+  maxAge: 30 * 60 * 1000
+});
 
 mailin.start({
   port: 25,
   disableWebhook: true,
   logLevel: 'silly',
   smtpOptions: {
-    SMTPBanner: 'Hi from a custom Mailin instance'
+    SMTPBanner: 'Hi from the SMTP test server'
   }
 });
 
 mailin.on('authorizeUser', function(connection, username, password, done) {
-  if (username === 'nicolas' && password === 'iznogoud') {
+  if (username === 'yacine' && password === 'secret') {
     done(null, true);
   } else {
     done(new Error('Unauthorized!'), false);
@@ -18,5 +26,22 @@ mailin.on('authorizeUser', function(connection, username, password, done) {
 });
 
 mailin.on('message', function (connection, data, content) {
-  console.log(data);
+  cache.set(connection.id, data);
+  console.log('EMAIL RECEIVED AND CACHED', connection.id);
 });
+
+app.get('/check', (req, res, next) => {
+  res.send('200 OK');
+});
+
+app.get('/emails/:emailId', (req, res, next) => {
+  const email = cache.get(req.params.emailId);
+
+  if (email) {
+    res.send(email);
+  } else {
+    res.status(404).send('Sorry, we cannot find that email!');
+  }
+});
+
+export default app;
